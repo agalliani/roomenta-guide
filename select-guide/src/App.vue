b41f575089251a342e08292a487cb5ce34ce8a45
Andrea Galliani <andrea.galliani.29@gmail.com>
2020-11-12 22:45:48 +0100

    Fixed some css

--- select-guide/src/App.vue
+++ select-guide/src/App.vue
@@ -3,61 +3,57 @@
     <div id="nav">
       <div id="a">
         <router-link to="/eng"
-          ><img class="flag-icon" src="./assets/great-britain-48.png"
+          ><img src="./assets/great-britain-48.png"
         /></router-link>
-        <router-link to="/ita"
-          ><img class="flag-icon" src="./assets/italy-48.png"
-        /></router-link>
+        <router-link to="/ita"><img id="it" src="./assets/italy-48.png"/></router-link>
       </div>
     </div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
 #nav {
-  left: 50%;
-  top: 1.5%;
-
-  position: absolute;
-  z-index: 1001;
+      display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  position: fixed;
+  top: 0;
+  left: 50px;
+  width: 100%;
+  text-align: center;
+  height: 64px;
+  padding: 0 36px 0 16px;
+  /* background-color: rgba(255, 0, 0, 0.2); */
+  z-index: 1002;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  position: relative;
-  left: -50%;
-}
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  border: 1px solid #021a40;
+  background-color: #f37021;
+  border-radius: 5px;
 }
 
-.flag-icon {
-  height: 4.5vh;
+img{
+    height: 40px;
 }
 
 @media screen and (max-width: 768px) {
   #nav {
-    top: 1.5%;
-    left: 75%;
-    position: absolute;
-    z-index: 1001;
+    text-align: right;
+ 
   }
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-    position: relative;
-    left: -50%;
+  #a{
+     margin-right: 50px;
+
   }
 }
 </style>
